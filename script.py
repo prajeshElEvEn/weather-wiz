e6f142f9a804603c9af876dfdfb9d32cf9ce613f
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-06-02 12:07:10 +0530

    added script

---
+++ script.py
@@ -0,0 +1,42 @@
+import requests
+
+
+def get_weather(city, api_key):
+    base_url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric"
+    }
+
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if data["cod"] == "404":
+            print("City not found.")
+        else:
+            weather = data["weather"][0]["description"]
+            temperature = data["main"]["temp"]
+            humidity = data["main"]["humidity"]
+            wind_speed = data["wind"]["speed"]
+
+            print(f"Weather: {weather}")
+            print(f"Temperature: {temperature}°C")
+            print(f"Humidity: {humidity}%")
+            print(f"Wind Speed: {wind_speed} m/s")
+
+    except requests.exceptions.RequestException as e:
+        print("An error occurred while retrieving the weather data.")
+        print(e)
+
+
+def main():
+    city = input("Enter the city name: ")
+    api_key = "YOUR_OPENWEATHER_API_KEY"
+
+    get_weather(city, api_key)
+
+
+if __name__ == "__main__":
+    main()
